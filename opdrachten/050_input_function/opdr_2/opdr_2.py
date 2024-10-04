b2ba25e765d04a9b890de63dd4acfa1fa60a2a16
AVEK30 <rune.joling@gmail.com>
2024-10-04 19:30:20 +0200

    opdracht 2 050 deels af

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -2,6 +2,22 @@
 # Naam student:
 # Groep:
 
-# Hier komt je code...
 
-gasten = ["Jij", ... ]
+stop = True
+gasten = ["rune", "paul", "kees", "marie", "hilda"]
+while stop:
+    tvvwst = (input("wil je een gast van of aan de lijst toevoegen of verwijderen? typ ""toevoegen"" of ""verwijderen"" wilt u stoppen typ ""stop"" \n"))
+
+    if tvvwst== 'toevoegen':
+        n_gast = input("voeg hier een gast toe \n")
+        gasten.append(n_gast)
+        print(gasten)
+
+    if tvvwst== 'verwijderen':
+        vw = input("typ hier de gast in die je wilt verwijderen\n")
+        gasten.remove(vw)
+        print(f"de gast is weggehaald hier is de nieuwe lijst {gasten}")
+
+    if tvvwst== 'stop': 
+        print (f"dit zijn alle gasten op de lijst {gasten}")
+        stop = False
